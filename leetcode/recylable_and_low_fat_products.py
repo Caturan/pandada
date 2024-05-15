d22647f089727730ae1debb0caeffade674efa38
Cevdet Ahmet TURAN <ahmetcevdetturan04@hotmail.com>
2024-05-15 12:20:08 +0300

    3.13

---
+++ leetcode/recylable_and_low_fat_products.py
@@ -0,0 +1,28 @@
+"""
+    Write a solution to find the ids of products that are both low fat and recyclable.
+
+    Return the result table in any order.
+
+    Input: 
+    Products table:
+    +-------------+----------+------------+
+    | product_id  | low_fats | recyclable |
+    +-------------+----------+------------+
+    | 0           | Y        | N          |
+    | 1           | Y        | Y          |
+    | 2           | N        | Y          |
+    | 3           | Y        | Y          |
+    | 4           | N        | N          |
+    +-------------+----------+------------+
+    Output: 
+    +-------------+
+    | product_id  |
+    +-------------+
+    | 1           |
+    | 3           |
+    +-------------+
+"""
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    return products.loc[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y'), ['product_id']] 
